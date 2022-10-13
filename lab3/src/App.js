9a490ee88b0728a10be511fd752c5c4d7f8fc95e
kirad26 <105429600+kirad26@users.noreply.github.com>
2022-10-13 10:21:22 +0300

    cards

---
+++ lab3/src/App.js
@@ -0,0 +1,71 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="result">
+        <Task4 markets={markets}/>
+    </div>
+  );
+}
+const markets = [
+  {
+    image: 'https://content1.rozetka.com.ua/goods/images/preview/25088160.jpg', 
+    cost: '383', 
+    info: '10м USB 2.0 активний подовжувач репітер'
+  },
+  {
+    image: 'https://content2.rozetka.com.ua/goods/images/preview/216522468.jpg', 
+    cost: '720', 
+    action: '1 039', 
+    info: 'Чоловіча сумка SumWin 2345MJ Чорна'
+  },
+  {
+    image: 'https://content2.rozetka.com.ua/goods/images/preview/264186522.jpg', 
+    cost: '3 499', 
+    action: '2 950', 
+    info: 'Графічний планшет XP-Pen Deco 01 V2 Чорний', 
+    active: true
+  },
+  
+  
+];
+function Task4(market)
+{
+  const cards = market.markets.map((info) =>
+    <div class='card'>
+      <div class='card-image'>
+        <a href='#'><img src={info.image} alt={info.info}></img></a>
+      </div>
+      <div class='card-information'>
+        <a href='#'><h4>{info.info}</h4></a>
+        {info.action != null && 
+         <span class='action'>{info.action} ₴</span>
+        }
+        <p>{info.cost} ₴</p>
+        {info.active === true &&
+          <span class='active'>Немає в наявності</span>
+        }
+      </div>
+    </div>
+  )
+  const forms = (
+    <form>
+      <input type='submit' value='показать больше'></input>
+    </form>
+  )
+  const titles = (
+    <h2>Заголовок</h2>
+  )
+  return(
+    <div class='container'>
+      {titles}
+      <div class='wrapper'>
+        {cards}
+      </div>
+      {forms}
+    </div>
+
+  );
+}
+export default App;
